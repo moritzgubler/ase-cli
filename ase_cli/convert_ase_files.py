69eef9c1b42be98c54fbd7221e9083ef20a99855
Moritz Gubler <moritz.gubler@gmail.com>
2024-09-02 09:37:07 +0200

    add ase file converter

---
+++ ase_cli/convert_ase_files.py
@@ -0,0 +1,28 @@
+from ase.io import read, write
+import argparse
+from os.path import basename
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Converts a file from one ASE format to another.')
+    parser.add_argument('input', type=argparse.FileType('r'), nargs="+", help='The file(s) to convert.')
+    parser.add_argument('--output', type=str, help='The output file. Only makes sense if only one input file is provided', required=False)
+    parser.add_argument("--input_format", type=str, help="The input format. Default: ase will choose format based on ending", required = False, default = "")
+    parser.add_argument("--index", type=str, help="The index of the file to convert, default 0", required = False, default = "0")
+    parser.add_argument("--format", type=str, help="The output format. Is superseded by the output option. Default: extxyz", required = False, default = "extxyz")
+    args = parser.parse_args()
+    
+    if args.output is not None and len(args.input) > 1:
+        raise ValueError("Specifying utput file only makes sense if only one input file is provided.")
+    
+    for i, f in enumerate(args.input):
+        if args.output is not None:
+            out = args.output
+        else:
+            out = basename(f.name) + "." + args.format
+        atoms = read(f, index=args.index, format=args.input_format)
+        f.close()
+        write(out, atoms, format=args.format)
+
+if __name__ == "__main__":
+    main()
