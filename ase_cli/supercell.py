9b9c2e30874e02ec8c6c89d0f0c812d8cc638200
moritzgubler <moritz.gubler@gmail.com>
2024-09-04 13:02:46 +0200

    add supercell program

---
+++ ase_cli/supercell.py
@@ -0,0 +1,20 @@
+import argparse
+from ase.io import read, write
+import os
+
+def parse(subparser):
+    parser = subparser.add_parser("supercell", help="Creates a supercell of a file.")
+    parser.add_argument('filename', type=argparse.FileType('r'), help='The file to create a supercell of.')
+    parser.add_argument('repeats', type=int, nargs=3, help='The number of repeats in each direction.')
+    parser.add_argument('--output', type=str, help='The output file. Default: input file with _supercell appended.', required=False)
+    parser.set_defaults(func=main)
+
+def main(args):
+    atoms = read(args.filename)
+    args.filename.close()
+    atoms.repeat(args.repeats)
+    if args.output is None:
+        output = os.path.splitext(args.filename.name)[0] + "_supercell" + os.path.splitext(args.filename.name)[1]
+    else:
+        output = args.output
+    write(output, atoms)
